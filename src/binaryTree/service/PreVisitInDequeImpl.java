e602128819c55dc6a5c9dfff326f6ea1bad9817c
Batman007 <sunchao@Batman-2.local>
2018-12-21 16:02:25 +0800

    二叉树前序非递归遍历使用队列实现

--- src/binaryTree/service/PreVisitInDequeImpl.java
+++ src/binaryTree/service/PreVisitInDequeImpl.java
@@ -6,15 +6,16 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * Created by sunchao on 2018/12/19.
+ * Created by Batman on 2018/12/19.
  * @author Batman
  * 非递归前序遍历二叉树 使用队列实现
  */
 public class PreVisitInDequeImpl implements Visit {
     @Override
     public void visitTree(TreeNode root) {
-        if(root == null)
+        if(root == null){
             return;
+        }
         Deque<TreeNode> deque = new ArrayDeque<>();
         while(root != null || deque.size() > 0){
             while (root != null){
