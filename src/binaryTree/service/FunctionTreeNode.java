9558201f65c85b1aba3f38a23cb9c858401cf865
Batman007 <sunchao@Batman-2.local>
2019-01-03 17:36:51 +0800

    添加反转二叉树的程序代码 LeetCode 226

--- src/binaryTree/service/FunctionTreeNode.java
+++ src/binaryTree/service/FunctionTreeNode.java
@@ -646,4 +646,22 @@ public class FunctionTreeNode {
         inorder(node.right, list);
     }
 
+    /**
+     * 反转二叉树
+     */
+    public TreeNode invertTree(TreeNode root){
+       if(root != null){
+           TreeNode temp = root.left;
+           root.left = root.right;
+           root.right = temp;
+       }
+        if(root.left != null){
+            invertTree(root.left);
+        }
+        if(root.right != null){
+            invertTree(root.right);
+        }
+        return root;
+    }
+
 }
