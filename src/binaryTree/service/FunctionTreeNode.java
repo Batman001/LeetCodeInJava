3fcc431752e54273207b4ac10b69f740b5b8fa35
Batman007 <sunchao@Batman-2.local>
2019-01-11 14:35:01 +0800

    add some funtion of leetcode with TreeNode structure

--- src/binaryTree/service/FunctionTreeNode.java
+++ src/binaryTree/service/FunctionTreeNode.java
@@ -756,4 +756,26 @@ public class FunctionTreeNode {
         convert(cur.left);
     }
 
+    /**
+     * LeetCode 572 Subtree of Another Tree
+     * Given two non-empty binary trees s and t,
+     * check whether tree t has exactly the same structure
+     * and node values with a subtree of s.
+     * A subtree of s is a tree consists of a node in s
+     * and all of this node's descendants.
+     * The tree s could also be considered as a subtree of itself.
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubtree(TreeNode s, TreeNode t){
+        if(s == null){
+            return false;
+        }
+        if(isSameTree(s, t)){
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
 }
