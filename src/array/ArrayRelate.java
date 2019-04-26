49af1c740ce8365d9eadf3e732c8c5d3f4d3a238
Batman007 <sun_thinker@163.com>
2019-04-26 16:59:10 +0800

    update code in array

--- src/array/ArrayRelate.java
+++ src/array/ArrayRelate.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 /**
  * @author Batman create on 2019-04-19 16:33
  */
@@ -29,6 +31,9 @@ public class ArrayRelate {
     /**
      * @author Batman create on 2019-03-18 11:25
      * Leetcode 27 Remove Element
+     * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
      */
     private int removeElement(int[] nums, int val){
         int count = 0;
@@ -39,6 +44,7 @@ public class ArrayRelate {
                 nums[i-count] = nums[i];
             }
         }
+        System.out.println(Arrays.toString(nums));
         return nums.length - count;
 
     }
