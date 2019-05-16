cee958beeffe8110d8f701f56bc5347a34ec629f
Batman007 <sun_thinker@163.com>
2019-05-16 10:13:05 +0800

    update some description of leetcode 438

--- src/string/FindAnagrams.java
+++ src/string/FindAnagrams.java
@@ -8,6 +8,27 @@ import java.util.List;
  * @author Batman
  * add a code from branch feature and master
  * FindAnagrams 找出字符串中所有的变位词 LeetCode 438
+ *
+ * 题目描述：
+ * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+ *
+ * 字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+ *
+ * 说明：
+ *
+ * 字母异位词指字母相同，但排列不同的字符串。
+ * 不考虑答案输出的顺序。
+ *
+ * 示例：
+ * 输入:
+ * s: "cbaebabacd" p: "abc"
+ *
+ * 输出:
+ * [0, 6]
+ *
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
  * 滑动窗口思想
  */
 public class FindAnagrams {
@@ -99,8 +120,8 @@ public class FindAnagrams {
     public static void main(String[] args) {
         String s = "cbaebabacd";
         String p = "abc";
-//        List<Integer> result = new FindAnagrams().findAnagrams(s, p);
-        List<Integer> result = new FindAnagrams().findAnagrams2(s, p);
+        List<Integer> result = new FindAnagrams().findAnagrams(s, p);
+//        List<Integer> result = new FindAnagrams().findAnagrams2(s, p);
         System.out.println(result);
     }
 }
